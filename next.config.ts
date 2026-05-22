954d66af79739cef410f8f6d5eb2bc5c3d8591c4
Juankos0714 <juankos0714@gmail.com>
2026-05-21 20:12:39 -0500

    hotfix vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    typedRoutes: true,
-  },
-  images: {
+images: {
     remotePatterns: [
       {
         protocol: "https",
